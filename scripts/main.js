5b7c6a386b30f58b51c346bc8115b81625ed3346
Thomas Bizet <thomas.bzt@gmail.com>
2023-11-16 11:44:38 +0100

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,3 @@
-const inputs = document.querySelectorAll(".fields");
-
-inputs.forEach(input => input.addEventListener("keyup", function (e) {
+$( 'input' ).on( 'input', function() {
 	document.getElementById("container").innerHTML = "<div><p>Ne donnez jamais vos identifiants et mots de passe à un site web ou une application.<p></div></br>"
-}));
+});

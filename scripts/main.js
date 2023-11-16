ed59db5ab2498778f7b6a739832107ab48063e7d
Thomas Bizet <thomas.bzt@gmail.com>
2023-11-16 14:06:07 +0100

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,4 @@
 $( 'input' ).on( 'input', function() {
+	$( 'input' ).value = "";
 	document.getElementById("container").innerHTML = "<div><p>Ne donnez jamais vos identifiants et mots de passe à un site web ou une application.<p></div></br>"
 });

4e6b35be7130abc83ee3e9ad3aca001d5f023533
Thomas Bizet <thomas.bzt@gmail.com>
2023-11-16 10:53:49 +0100

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,5 @@
-let baseUrl = "https://api.ecoledirecte.com/v3";
-const body = document.getElementsByTagName("body")[0];
-let fields;
-const submitButton = document.getElementById("submit");
 const inputs = document.querySelectorAll(".fields");
 
 inputs.forEach(input => input.addEventListener("keypress", function (e) {
-	alert("NE DONNEZ JAMAIS VOS IDENTIFIANTS ET MOTS DE PASSE !");
+	document.getElementById("container").innerHTML = "<div><p>Ne donnez jamais vos identifiants et mots de passe à un site web ou une application.<p></div></br>"
 }));
